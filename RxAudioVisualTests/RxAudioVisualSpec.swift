b626ec4b7d2e1f806e840e7659598b1c9199a025
keita.oouchi <keita.oouchi@gmail.com>
2017-06-01 14:48:27 +0900

    WIP

---
+++ RxAudioVisualTests/RxAudioVisualSpec.swift
@@ -0,0 +1,14 @@
+import Quick
+import Nimble
+
+class RxAudioVisualSpec: QuickSpec {
+
+  override func spec() {
+    describe("test") {
+      it("hoge") {
+        expect(1).to(equal(2))
+      }
+    }
+  }
+    
+}
